ae52558ec1dd4132dc75beea56031ce5789c066b
Gerald Craig <geraldcraig@Geralds-MacBook-Pro.local>
2020-09-30 23:00:41 +0100

    week 2 added

---
+++ Year1/src/uk/ac/qub/week1/Practical2c.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.ac.qub.week1;
+
+/**
+ * @author geraldcraig
+ *
+ */
+public class Practical2c {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
