eb39a75333c2577604181828ba00a0f6c81f2599
Gerald Craig <geraldcraig@Geralds-Mac-mini.local>
2020-10-08 20:38:41 +0100

    loop demo added

---
+++ Year1/src/uk/ac/qub/week3/ForLoopDemo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package uk.ac.qub.week3;
+
+/**
+ * @author geraldcraig
+ *
+ */
+public class ForLoopDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println("Counter " + i);
+			
+		}
+	}
+
+}
