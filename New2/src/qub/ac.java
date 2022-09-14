46e12fb975f11a49905711fb526a981eb9af57ba
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-09-14 21:30:26 +0100

    idea updated

---
+++ New2/src/qub/ac.java
@@ -0,0 +1,7 @@
+package qub;
+
+public class ac {
+    public static void main(String[] args) {
+        System.out.println(1 + 3);
+    }
+}
---
+++ Num/src/main/java/qub/com.java
@@ -0,0 +1,7 @@
+package qub;
+
+public class com {
+    public static void main(String[] args) {
+        System.out.println(6 +9);
+    }
+}
