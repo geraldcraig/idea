835a034b00bf4a86d3117118bf1a0f9c4783c2fa
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-09-14 18:49:26 +0100

    spring boot added

---
+++ PythonProject/encrypt.py
@@ -0,0 +1,40 @@
+"""
+Caesar Cipher
+Create a simple cipher generator – when a word is entered, 
+it will convert it to ciphertext based on a given ‘key’ 
+based on your birth month (01 to 12).
+"""
+
+
+# function to encrypt phrase using birth month number as the key
+def encryption(text, key):
+    cipher = ''
+    for i in text:
+        if i.isupper():
+            temp = 65 + ((ord(i) - 65 + key) % 26)
+            cipher = cipher + chr(temp)
+        elif i.islower():
+            temp = 97 + ((ord(i) - 97 + key) % 26)
+            cipher = cipher + chr(temp)
+        else:
+            cipher = cipher + i
+    print("The ciphertext is:", cipher)
+
+
+# list to store all the months of the year
+months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november",
+          "december"]
+
+# variables to store user input phrase and birth month
+phrase = input("Enter phrase to encrypt: ")
+birthMonth = input("Enter your birth month: ")
+
+# while loop to get index of birth month
+while True:
+    birthMonth = birthMonth.lower()
+    if birthMonth in months:
+        monthKey = months.index(birthMonth) + 1
+        encryption(phrase, monthKey)
+        break
+    else:
+        birthMonth = input("Sorry not recognised, please enter a valid month: ")
