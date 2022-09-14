184cb8d1723f1a27c66e56beb776bd56a1cd5767
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-09-14 18:02:46 +0100

    python added

---
+++ Day01/src/exercise02/Challenge.java
@@ -0,0 +1,24 @@
+package exercise02;
+
+import java.util.Scanner;
+
+public class Challenge {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void numKilograms(int pounds) {
+        double kilograms = 0.453594237;
+        System.out.println(pounds * kilograms);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of pounds: ");
+        int input = sc.nextInt();
+
+        numKilograms(input);
+
+        sc.close();
+
+    }
+}
