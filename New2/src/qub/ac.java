4ef73ac9fe6c39c313f0564f4253fb8a7bafec2c
geraldcraig <geraldcraig@me.com>
2022-09-15 15:26:05 +0100

    updated

--- New2/src/qub/ac.java
+++
@@ -1,7 +0,0 @@
-package qub;
-
-public class ac {
-    public static void main(String[] args) {
-        System.out.println(1 + 3);
-    }
-}
--- Num/src/main/java/qub/com.java
+++
@@ -1,7 +0,0 @@
-package qub;
-
-public class com {
-    public static void main(String[] args) {
-        System.out.println(6 +9);
-    }
-}
