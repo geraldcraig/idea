54e0fe57b012ee990e8ef8b91f172dd7194b8961
geraldcraig <geraldcraig@me.com>
2022-09-26 14:45:06 +0100

    updated

--- Day04/src/exercise01/BarkingDog.java
+++ Day04/src/exercise01/BarkingDog.java
@@ -12,11 +12,9 @@ public class BarkingDog {
 
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
 
-        if (hourOfDay < 0 || hourOfDay > 23) {
-            return false;
-        } else if (barking == true && (hourOfDay <= 8 || hourOfDay > 22)) {
+        if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
             return true;
         }
-            return false;
+        return false;
     }
 }
