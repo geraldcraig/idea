4729cd35413916b870d728ef0006860cba788fd0
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-10-06 21:20:56 +0100

    day 6 updated

--- Day06/src/exercise1/ReadUserData.java
+++ Day06/src/exercise1/ReadUserData.java
@@ -1,8 +1,22 @@
 package exercise1;
 
-public class ReadUserData {
+import java.util.Scanner;
+
+public class  ReadUserData {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int userInput;
+        do {
+            System.out.println("enter number");
+            count++;
+        } while (count < 10);
+        userInput = sc.nextInt();
+        count++;
+        System.out.println(count);
+
+        sc.close();
 
     }
 }
