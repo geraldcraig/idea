85a170f44edc3ec17ebcee7f02c3e761ee603a6c
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-10-11 08:30:22 +0100

    practically java added

---
+++ PracticallyJava/src/com/gerrybyrne/Example1.java
@@ -0,0 +1,20 @@
+package com.gerrybyrne;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("--------Build your Java muscle--------");
+        System.out.println("----------Learn To Code------------");
+        System.out.println();
+        System.out.println();
+        System.out.println("Press any letter on the keyboard to continue");
+        myScanner.next();
+        System.out.println("Goodbye");
+        myScanner.close();
+    }
+}
