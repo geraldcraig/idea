ddec1908b6c99fa7b535c8903c56a817507c4baf
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-10-28 18:59:29 +0100

    lab 5 updated

--- PracticallyJava/src/com/labs/module05/Lab1.java
+++ PracticallyJava/src/com/labs/module05/Lab1.java
@@ -1,6 +1,14 @@
 package com.labs.module05;
 
 public class Lab1 {
+    /*
+    Write a Java console application that will calculate and display the number of points accumulated by a
+    sports team during their season. The program should ask the user to input the number of games won,
+    the number of games drawn and the number of games lost. The program should total the number of games
+    played and calculate the number of points won based on the facts that 3 points are given for a win,
+    1 point is given for a draw and 0 points are given for a lost game. Display the number of games played
+    and the number of points accumulated.
+     */
 
     public static void main(String[] args) {
 
--- PracticallyJava/src/com/labs/module05/Lab2.java
+++ PracticallyJava/src/com/labs/module05/Lab2.java
@@ -1,6 +1,14 @@
 package com.labs.module05;
 
 public class Lab2 {
+    /*
+    the total score for two tests that a student undertakes
+
+    the average of the two scores
+
+    the two scores will be input by the user at the console and will be accepted as String
+    values by the program code (conversion will be needed)
+     */
 
     public static void main(String[] args) {
 
