f06414f08e652fd3e713ffdda21991a85e90ae23
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-11-10 22:36:25 +0000

    processexec added

---
+++ AudioScripts/src/scripts/ProcessExec.java
@@ -0,0 +1,36 @@
+package scripts;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ProcessExec {
+
+    public static void main(String[] args) {
+        String s = null;
+        String input = "audio_example.mp3";
+        try {
+            // Process provides control of native processes started by ProcessBuilder.start and Runtime.exec.
+            // getRuntime() returns the runtime object associated with the current Java application.
+            //Process p = Runtime.getRuntime().exec("spleeter separate -p spleeter:5stems -o output " + input);
+            Process p = Runtime.getRuntime().exec("python3 hello.py " );
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
---
+++ AudioScripts/src/scripts/Script.java
@@ -0,0 +1,39 @@
+package scripts;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Script {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+        String[] command = {"python3", "hello.py"};
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        processBuilder.directory(new File(System.getProperty("user.home")));
+
+        try {
+            Process process = processBuilder.start();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+
+            System.out.println("\nExited with error code : " + exitCode);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
