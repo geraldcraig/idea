df18abd93c211c4cf206c5c12535dcb4531694fb
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-11-13 07:47:15 +0000

    updated

--- AudioProject/src/main/java/com/example/audioproject/GreetingController.java
+++ AudioProject/src/main/java/com/example/audioproject/GreetingController.java
@@ -1,5 +1,6 @@
-package com.example.restservice;
+package com.example.audioproject;
 
+import com.example.audioproject.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
